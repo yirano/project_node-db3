c7437cf75e1c292eb15deb72026d7860bbdce070
yirano <yirano@protonmail.ch>
2020-07-27 23:46:01 -0400

    Correct query

--- queries.sql
+++ queries.sql
@@ -7,12 +7,12 @@ JOIN "Category" AS c
 WHERE p."CategoryId" = c."Id"
 ;
 
--- ! Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+-- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT o."Id", s."CompanyName"
 FROM "Order" AS o
 JOIN "Shipper" AS s
 WHERE o."ShipVia" = s."Id"
-AND o."ShippedDate" < '2012-08-09'
+AND o."OrderDate" < '2012-08-09'
 ;
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
