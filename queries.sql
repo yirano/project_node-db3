ce6be17bfda3950db3c7b1dd3f158ed1cbf2aa03
yirano <yirano@protonmail.ch>
2020-07-28 22:54:29 -0400

    Completed SQL Stretch

--- queries.sql
+++ queries.sql
@@ -3,25 +3,25 @@
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 SELECT p."ProductName", c."CategoryName"
 FROM "Product" AS p
-JOIN "Category" AS c
+  JOIN "Category" AS c 
 WHERE p."CategoryId" = c."Id"
 ;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT o."Id", s."CompanyName"
 FROM "Order" AS o
-JOIN "Shipper" AS s
+  JOIN "Shipper" AS s 
 WHERE o."ShipVia" = s."Id"
-AND o."OrderDate" < '2012-08-09'
+  AND o."OrderDate" < '2012-08-09'
 ;
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 SELECT p."ProductName", o."Quantity"
 FROM "ProductDetails_V" AS p
-JOIN "OrderDetail" AS o
+  JOIN "OrderDetail" AS o 
 WHERE o."ProductId" = p."Id"
-AND o."OrderId" = 10251
+  AND o."OrderId" = 10251
 ORDER BY p."ProductName"
 ;
 
@@ -37,10 +37,19 @@ ORDER BY p."ProductName"
 -- ;
 
 SELECT o."Id" AS "Order ID",
-c."CompanyName" AS "Customer's Company",
-e."LastName" AS "Employee's Lastname",
+  c."CompanyName" AS "Customer's Company",
+  e."LastName" AS "Employee's Lastname", 
 FROM "Order" AS o
-JOIN "Customer" AS c, "Employee" as e
+  JOIN "Customer" AS c   , "Employee" as e
 WHERE c."Id" = o."CustomerId"
-AND o."EmployeeId" = e."Id"
+  AND o."EmployeeId" = e."Id"
 ;
+
+
+--- SQL Stretch ---
+
+SELECT "Categories"."CategoryName", 
+ALTER TABLE "Categories"
+ADD "Count" INT
+; 
+
